03be40287f4df13ecd6c3a7a2c5d4009c980f02f
MarkoJauregui <markojau@gmail.com>
2024-10-26 15:36:25 -0300

    Added chars_list and printed report

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
     chars_dict = get_chars_dict(text)
-    print(chars_dict)
+    sorted_chars_dict = get_chars_list(chars_dict)
+    print(f"--- Begin Report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    for char_dict in sorted_chars_dict :
+        print(f"The {char_dict['char']} character was found {char_dict['num']} times")
+    print("--- End report ---")
 
 
 def get_num_words(text):
@@ -22,6 +26,18 @@ def get_chars_dict(text):
     return chars
 
 
+def sort_on(dict):
+    return dict["num"]
+
+def get_chars_list(chars_dict):
+    chars_list = []
+    for char, num in chars_dict.items():
+        if char.isalpha():
+            char_dict = {"char": char, "num": num}
+            chars_list.append(char_dict)
+
+    chars_list.sort(reverse=True, key=sort_on)
+    return chars_list
 
 def get_book_text(path):
     with open(path) as f:
